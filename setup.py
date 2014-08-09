8171e09e127d8c2c8c1090d6d2193684ef34f49d
Robpol86 <robpol86@gmail.com>
2014-08-08 22:07:34 -0700

    Sorted imports, added testcovweb.
    Sorted imports according to PyCharm.
    Added testcovweb command to setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import ast
-from codecs import open  # To use a consistent encoding
+import atexit
+from codecs import open
+from distutils.spawn import find_executable
 import os
+import sys
+import subprocess
+
 import setuptools
 import setuptools.command.sdist
 from setuptools.command.test import test
-import sys
 
 
 setattr(setuptools.command.sdist, 'READMES',
@@ -41,6 +45,18 @@ class PyTestCov(PyTest):
         test.finalize_options(self)
         setattr(self, 'test_args', ['--cov', 'flask_statics', 'tests'])
         setattr(self, 'test_suite', True)
+
+
+class PyTestCovWeb(PyTest):
+    def finalize_options(self):
+        test.finalize_options(self)
+        setattr(self, 'test_args', ['--cov-report', 'html', '--cov', 'flask_statics', 'tests'])
+        setattr(self, 'test_suite', True)
+
+    def run_tests(self):
+        if find_executable('open'):
+            atexit.register(lambda: subprocess.call(['open', os.path.join(here, 'htmlcov', 'index.html')]))
+        PyTest.run_tests(self)
 
 
 # Setup definition.
@@ -101,5 +117,5 @@ setuptools.setup(
     install_requires=['Flask'],
 
     tests_require=['pytest'],
-    cmdclass=dict(test=PyTest, testcov=PyTestCov),
+    cmdclass=dict(test=PyTest, testcov=PyTestCov, testcovweb=PyTestCovWeb),
 )
