c4222241218d2ba20bb9685bdffd1dc5e38b3369
Robpol86 <robpol86@gmail.com>
2014-08-09 18:10:03 -0700

    Another Python3.4 attempt.
    This one worked locally.

--- tests/test_consistency.py
+++ tests/test_consistency.py
@@ -1,6 +1,5 @@
 from codecs import open
 import os
-import random
 import re
 from string import Template
 
@@ -37,7 +36,7 @@ def test_files_reachable_from_template(minify):
     remote_file = lambda x: '/static/flask_statics_helper/{0}'.format(x)
 
     # Create Flask app.
-    app = Flask('{0}_{1}'.format(__name__, str(random.randint(1, 100))))
+    app = Flask(__name__)
     app.config['STATICS_MINIFY'] = minify
     Statics(app)
     all_resources = ((k, set(v['css']), set(v['js']))
